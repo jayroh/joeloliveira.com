f2bd0558bd99b34a495da4f970e444d4bd219708
Joel Oliveira <joel.oliveira@gmail.com>
2022-06-07 18:46:50 -0400

    Remove jekyll-imgproxy-tag source from vendor
    Refer to the just-published gem here: https://rubygems.org/gems/jekyll-imgproxy-tag
    
    The project has moved to https://github.com/jayroh/jekyll-imgproxy-tag/

--- vendor/jekyll-imgproxy-tag/lib/jekyll-imgproxy-tag.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'jekyll'
-require 'jekyll/imgproxy/tag'
-require 'jekyll/imgproxy/tag/imgproxy_config'
-require 'jekyll/imgproxy/tag/path_builder'
-require 'jekyll/imgproxy/tag/url_generator'
-require 'jekyll/imgproxy/tag/errors'
